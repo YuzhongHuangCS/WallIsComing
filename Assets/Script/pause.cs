9f6f3542dc90dfc13f55dd28cbe17d581c67c1ac
Yuzo <hyzgog@gmail.com>
2015-07-17 01:11:23 +0800

    submit scene

---
+++ Assets/Script/pause.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;  
+
+
+
+
+public class pause : MonoBehaviour , IPointerClickHandler{
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	bool i=true;
+
+	void OnPressed ()    
+	{    
+		i = !i;    
+	}   
+
+
+	void OnClick () {  
+		if (this.i) {  
+			object obj = Resources.Load("health", typeof(Sprite));
+			Sprite sp = obj as Sprite;
+
+			this.i = false;  
+		}  
+		else{  
+			object obj = Resources.Load("source", typeof(Sprite));
+			Sprite sp = obj as Sprite;
+ 
+			this.i = true;  
+		}  
+	}  
+
+	public void OnPointerClick (PointerEventData data) {
+
+			// reload the scene
+		OnPressed ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+		if (i) {
+			Time.timeScale = 1;
+		}
+		else {
+			Time.timeScale=0;
+		}
+	}
+}
+
+
